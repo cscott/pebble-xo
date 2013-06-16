b1e97db77056d44205b3f2f3105527841dc9000c
C. Scott Ananian <cscott@cscott.net>
2013-06-16 19:56:44 -0400

    Add time to XO clock app.

--- src/xo.c
+++ src/xo.c
@@ -7,10 +7,12 @@
 PBL_APP_INFO(MY_UUID,
              "XO Clock", "cscott.net",
              1, 0, /* App version */
-             DEFAULT_MENU_ICON,
-             APP_INFO_STANDARD_APP);
+             RESOURCE_ID_IMAGE_MENU_ICON,
+             APP_INFO_WATCH_FACE);
 
 Window window;
+
+TextLayer text_time_layer;
 
 BmpContainer image_container;
 
@@ -27,6 +29,14 @@ void handle_init(AppContextRef ctx) {
     bmp_init_container(RESOURCE_ID_IMAGE_XO_BACKGROUND, &image_container);
 
     layer_add_child(&window.layer, &image_container.layer.layer);
+
+    text_layer_init(&text_time_layer, window.layer.frame);
+    text_layer_set_text_color(&text_time_layer, GColorWhite);
+    text_layer_set_text_alignment(&text_time_layer, GTextAlignmentCenter);
+    text_layer_set_background_color(&text_time_layer, GColorClear);
+    layer_set_frame(&text_time_layer.layer, GRect(2, 168-23, 144-2, 168-3));
+    text_layer_set_font(&text_time_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_ROBOTO_BOLD_SUBSET_20)));
+    layer_add_child(&window.layer, &text_time_layer.layer);
 }
 
 void handle_deinit(AppContextRef ctx) {
@@ -37,10 +47,43 @@ void handle_deinit(AppContextRef ctx) {
     bmp_deinit_container(&image_container);
 }
 
+void handle_minute_tick(AppContextRef ctx, PebbleTickEvent *t) {
+
+  (void)ctx;
+
+  // Need to be static because they're used by the system later.
+  static char time_text[] = "00:00";
+
+  char *time_format;
+
+
+  if (clock_is_24h_style()) {
+    time_format = "%R";
+  } else {
+    time_format = "%I:%M";
+  }
+
+  string_format_time(time_text, sizeof(time_text), time_format, t->tick_time);
+
+  // Kludge to handle lack of non-padded hour format string
+  // for twelve hour clock.
+  if (!clock_is_24h_style() && (time_text[0] == '0')) {
+      time_text[0] = ' ';
+  }
+
+  text_layer_set_text(&text_time_layer, time_text);
+
+}
+
+
 void pbl_main(void *params) {
     PebbleAppHandlers handlers = {
         .init_handler = &handle_init,
-        .deinit_handler = &handle_deinit
+        .deinit_handler = &handle_deinit,
+        .tick_info = {
+            .tick_handler = &handle_minute_tick,
+            .tick_units = MINUTE_UNIT
+        }
     };
     app_event_loop(params, &handlers);
 }
